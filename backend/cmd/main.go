18e859a6efa98baf1786f90684394427cdc2595f
Alyosha <alohahoy@gmail.com>
2025-06-29 07:13:58 +0100

    cors issues

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -12,8 +12,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/alyosha-bar/medPortal/database"
 	"github.com/alyosha-bar/medPortal/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/alyosha-bar/medPortal/cmd/docs"
@@ -29,6 +32,15 @@ func main() {
 
 	// Set Up GIN router
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://diligent-perception-production.up.railway.app/"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Serve Swagger UI
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
